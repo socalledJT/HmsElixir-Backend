2a5314556a2bc6cd20ce1cce8bb809db227e8981
Saimir Brahja <SaimirBrahja@outlook.com>
2024-01-21 14:10:55 +0100

    Added user Entity

---
+++ src/main/java/org/sda/hms/entities/User.java
@@ -0,0 +1,26 @@
+package org.sda.hms.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.sda.hms.entities.enums.UserRole;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private UserRole role;
+}
