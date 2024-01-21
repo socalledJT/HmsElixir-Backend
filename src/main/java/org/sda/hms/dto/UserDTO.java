c3b4c3e8606a91a00caea18159131e35cb633811
Saimir Brahja <SaimirBrahja@outlook.com>
2024-01-21 15:03:48 +0100

    Added UserDTO

---
+++ src/main/java/org/sda/hms/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package org.sda.hms.dto;
+
+import org.sda.hms.entities.enums.UserRole;
+
+public class UserDTO {
+
+    private Integer id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private UserRole role;
+}
