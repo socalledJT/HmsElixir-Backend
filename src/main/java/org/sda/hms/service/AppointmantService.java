02033e3bf9d953c7ae6454e35f42fe23444d81bf
AndiBocova <148264722+AndiBocova@users.noreply.github.com>
2024-01-23 16:24:54 +0100

    appointments DTO + Converter + Service + ServiceImpl

--- src/main/java/org/sda/hms/service/AppointmantService.java
+++ src/main/java/org/sda/hms/service/AppointmantService.java
@@ -1,4 +1,15 @@
 package org.sda.hms.service;
 
+import org.sda.hms.dto.AppointmentDTO;
+import org.sda.hms.entities.Appointment;
+
+import java.util.List;
+
 public interface AppointmantService {
+
+    void save(AppointmentDTO appointmentDTO);
+    AppointmentDTO findById(Integer id);
+    void update(AppointmentDTO appointmentDTO);
+    void delete(Integer id);
+    List<AppointmentDTO> findAll();
 }
--- src/main/java/org/sda/hms/service/impl/AppointementServiceImpl.java
+++ src/main/java/org/sda/hms/service/impl/AppointementServiceImpl.java
@@ -1,6 +1,44 @@
 package org.sda.hms.service.impl;
 
+import org.sda.hms.dto.AppointmentDTO;
+import org.sda.hms.repository.AppointmentRepo;
 import org.sda.hms.service.AppointmantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+@Service
 public class AppointementServiceImpl implements AppointmantService {
+
+    @Autowired
+    private AppointmentRepo appointmentRepo;
+
+
+    @Override
+    public void save(AppointmentDTO appointmentDTO) {
+
+    }
+
+    @Override
+    public AppointmentDTO findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void update(AppointmentDTO appointmentDTO) {
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public List<AppointmentDTO> findAll() {
+        return null;
+    }
 }
+
