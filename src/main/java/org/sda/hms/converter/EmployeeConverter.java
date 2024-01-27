78b4ab051fe65f6de5133b74b64b8eb26dad2342
FikneteKopshti <kopshtin@gmail.com>
2024-01-27 11:02:14 +0100

    Employee

--- src/main/java/org/sda/hms/converter/EmployeeConverter.java
+++ src/main/java/org/sda/hms/converter/EmployeeConverter.java
@@ -2,13 +2,15 @@ package org.sda.hms.converter;
 
 import org.sda.hms.dto.EmployeeDto;
 import org.sda.hms.entities.Employee;
+import org.sda.hms.entities.User;
 
 public class EmployeeConverter {
     public static EmployeeDto toDto(Employee employee){
         EmployeeDto employeeDto= new EmployeeDto();
 
-
-
+        employeeDto.setId(employee.getId());
+        employeeDto.setUserId(employee.getUserId());
+        employeeDto.setDepartmentId(employee.getDepartmentId());
 
 
         return employeeDto;
