1bfefdff32fb4960cf74ce83dfc9a47d12dc0cc6
narilda <narildashehaj@hotmail.com>
2024-01-27 14:09:47 +0100

    Edit examination file

--- src/main/java/org/sda/hms/service/impl/ExaminationServiceImpl.java
+++ src/main/java/org/sda/hms/service/impl/ExaminationServiceImpl.java
@@ -1,11 +1,51 @@
 package org.sda.hms.service.impl;
 
-import jakarta.transaction.Transactional;
-
+import org.sda.hms.converter.ExaminationConverter;
+import org.sda.hms.dto.ExaminationDTO;
+import org.sda.hms.entities.Examination;
+import org.sda.hms.exeptions.NotFoundException;
+import org.sda.hms.repository.ExaminationRepository;
+import org.sda.hms.service.ExaminationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-@Transactional
-public class ExaminationServiceImpl {
+import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
+@Service
+
+public class ExaminationServiceImpl implements ExaminationService {
+    @Autowired
+    private ExaminationRepository examinationRepository;
+
+    @Override
+    public void save(ExaminationDTO examinationDTO) {
+        Examination examination = ExaminationConverter.toEntity(examinationDTO);
+        examinationRepository.save(examination);
+
+    }
+
+    @Override
+    public ExaminationDTO findById(Integer id) {
+        return ExaminationConverter.toDTO(examinationRepository.findById(id)
+                .orElseThrow(()-> new NotFoundException("This Examination does not exit!")));
+    }
+    @Override
+    public void update (ExaminationDTO examinationDTO){
+        Examination examination = examinationRepository.findById(examinationDTO.getId())
+                .orElseThrow(() -> new NotFoundException("Examination doesn't exist"));
+                examinationRepository.save(ExaminationConverter.toEntityForUpdate(examination, examinationDTO));
+    }
+    @Override
+    public void delete(ExaminationDTO examinationDTO) {
+        Examination examination = ExaminationConverter.toEntity(examinationDTO);
+        examinationRepository.delete(examination);
+    }
+
+    @Override
+    public List<ExaminationDTO> findAll() {
+        return examinationRepository.findAll().stream().map(ExaminationConverter::toDTO).toList();
+
+    }
 }
