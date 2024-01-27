f411cce9d941d93e23bbdbfa7c978897f61d5187
FikneteKopshti <kopshtin@gmail.com>
2024-01-27 08:49:02 +0100

    EmploeeDto , EmployeeService

--- src/main/java/org/sda/hms/service/EmployeeService.java
+++ src/main/java/org/sda/hms/service/EmployeeService.java
@@ -1,4 +1,8 @@
 package org.sda.hms.service;
 
+import org.sda.hms.dto.EmployeeDto;
+
 public interface EmployeeService {
+    void save(EmployeeDto employeeDto);
+    EmployeeDto findById(Integer id);
 }
