3f90cd8fcbbf558d7561970b43af0ea4bf7b6824
narilda <narildashehaj@hotmail.com>
2024-01-27 10:18:09 +0100

    Edit examination file

---
+++ src/main/java/org/sda/hms/controller/ExaminationController.java
@@ -0,0 +1,18 @@
+package org.sda.hms.controller;
+
+
+import org.sda.hms.dto.ExaminationDTO;
+import org.sda.hms.service.ExaminationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/examination")
+    public class ExaminationController {
+
+    }
+
+
