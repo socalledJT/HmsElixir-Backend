b617c19f520ad704f9a67cff0a34f908dabb5d7b
FikneteKopshti <kopshtin@gmail.com>
2024-01-27 08:49:02 +0100

    EmploeeDto , EmployeeService

--- src/main/java/org/sda/hms/dto/EmployeeDto.java
+++ src/main/java/org/sda/hms/dto/EmployeeDto.java
@@ -2,5 +2,6 @@ package org.sda.hms.dto;
 
 public class EmployeeDto {
     private Integer id;
+    private UserDTO userId;
 
 }
--- src/main/java/org/sda/hms/service/impl/EmployeeServiceImpl.java
+++ src/main/java/org/sda/hms/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,22 @@
 package org.sda.hms.service.impl;
 
+import org.sda.hms.dto.EmployeeDto;
 import org.sda.hms.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @Override
+    public void save(EmployeeDto employeeDto) {
+
+    }
+
+    @Override
+    public EmployeeDto findById(Integer id) {
+        return null;
+    }
 }
