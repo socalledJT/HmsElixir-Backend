c753756e998ed2b740284cdd4eadb7566e6e9d8e
FikneteKopshti <kopshtin@gmail.com>
2024-01-27 10:13:36 +0100

    Emploee

---
+++ src/main/java/org/sda/hms/converter/EmployeeConverter.java
@@ -0,0 +1,16 @@
+package org.sda.hms.converter;
+
+import org.sda.hms.dto.EmployeeDto;
+import org.sda.hms.entities.Employee;
+
+public class EmployeeConverter {
+    public static EmployeeDto toDto(Employee employee){
+        EmployeeDto employeeDto= new EmployeeDto();
+
+
+
+
+
+        return employeeDto;
+    }
+}
--- src/main/java/org/sda/hms/service/EmployeeService.java
+++ src/main/java/org/sda/hms/service/EmployeeService.java
@@ -4,5 +4,7 @@ import org.sda.hms.dto.EmployeeDto;
 
 public interface EmployeeService {
     void save(EmployeeDto employeeDto);
+    void update(EmployeeDto employeeDto);
     EmployeeDto findById(Integer id);
+    void delete(EmployeeDto employeeDto);
 }
