a74a00e4a0b650b905f3371556576ee73b6d75d2
Saimir Brahja <SaimirBrahja@outlook.com>
2024-01-27 11:12:23 +0100

    Updated TODO

--- src/main/java/org/sda/hms/service/impl/UserServiceImpl.java
+++ src/main/java/org/sda/hms/service/impl/UserServiceImpl.java
@@ -52,7 +52,7 @@ public class UserServiceImpl implements UserService {
     }
 
 //    TODO Add findByEmail function for the User entity
-//    @Override
+//    @Override-
 //    public UserDTO findByEmail(String email) {
 //        return UserConverter.toDto(userRepository.findByEmail(email));
 //    }
