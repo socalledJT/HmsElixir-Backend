2a26404714f1cd5a723fd38f7e3da939dd06673e
narilda <narildashehaj@hotmail.com>
2024-01-28 11:44:21 +0100

    Edit examination file

--- src/main/java/org/sda/hms/controller/ExaminationController.java
+++ src/main/java/org/sda/hms/controller/ExaminationController.java
@@ -1,6 +1,7 @@
 package org.sda.hms.controller;
 
 
+import org.sda.hms.dto.EmployeeDto;
 import org.sda.hms.dto.ExaminationDTO;
 import org.sda.hms.service.ExaminationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,28 @@ import java.util.List;
     @RestController
     @RequestMapping("/examination")
     public class ExaminationController {
+        @Autowired
+        private ExaminationService examinationService;
 
+        @PostMapping
+        public void save(@RequestBody ExaminationDTO examinationDTO) {
+            examinationService.save(examinationDTO);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<ExaminationDTO> findById(@PathVariable Integer id) {
+            return ResponseEntity.ok(examinationService.findById(id));
+        }
+        @PutMapping
+        public void update (@RequestBody ExaminationDTO examinationDTO){
+            examinationService.update(examinationDTO);
+
+        }
+        @GetMapping
+        public ResponseEntity <List<ExaminationDTO>> findAll(){
+            return ResponseEntity.ok(examinationService.findAll());
+        }
     }
+
 
 
